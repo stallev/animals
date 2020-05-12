c2df2a9ca82f13ca833b7b9c46a56e89245c817b
Alexander <stallev@yahoo.com>
2020-05-12 06:44:29 +0300

    section info-intro

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,25 @@
-let name = 'User Name';
-let place = 'Flavortown';
+'use strict';
 
-console.log( `Hello ${name}, ready for ${place}?` );
+(function () {
+  var hamburger = document.querySelector('.header__toggle-button--hamburger');
+  var cross = document.querySelector('.header__toggle-button--cross');
+  var nav = document.querySelector('.nav');
+  var upperHeader = document.querySelector('.header__upper');
+  var contactsBlock = document.querySelector('.header__contacts-block');
 
-svg4everybody();
+  hamburger.addEventListener('click', onDisplayNav);
+  cross.addEventListener('click', onCloseNav);
+
+  function onDisplayNav() {
+    nav.style.display = 'flex';
+    contactsBlock.style.display = 'flex';
+    hamburger.style.display = 'none';
+    cross.style.display = 'block';
+  }
+  function onCloseNav() {
+    nav.style.display = 'none';
+    contactsBlock.style.display = 'none';
+    hamburger.style.display = 'block';
+    cross.style.display = 'none';
+  }
+})();
